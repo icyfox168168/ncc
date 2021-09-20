5e024136b0cfd3655092fd0f1b598816ee5cda66
Charles E. Youse <charles@gnuless.org>
2021-09-20 10:20:13 -0400

    cc1: build skeleton for compiler pass 1
    Signed-off-by: Charles E. Youse <charles@gnuless.org>

---
+++ cc1/cc1.cc
@@ -0,0 +1,13 @@
+/* cc1.cc - compiler pass 1                        ncc, the new c/c++ compiler
+
+                    Copyright (c) 2021 Charles E. Youse (charles@gnuless.org).
+                    All rights reserved. See LICENSE file for more details. */
+
+#include "cc1.h"
+
+int main()
+{
+    return 0;
+}
+
+/* vi: set ts=4 expandtab: */
---
+++ cc1/cc1.h
@@ -0,0 +1,11 @@
+/* cc1.h - compiler pass 1                         ncc, the new c/c++ compiler
+
+                    Copyright (c) 2021 Charles E. Youse (charles@gnuless.org).
+                    All rights reserved. See LICENSE file for more details. */
+
+#ifndef CC1_H
+#define CC1_H
+
+#endif /* CC1_H */
+
+/* vi: set ts=4 expandtab: */
